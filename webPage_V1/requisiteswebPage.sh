c85c84bbcadca13d8e1aa1ef9364b2d99c3b28a4
aritzM <baritzmg247@gmail.com>
2021-02-18 20:18:56 +0000

    Requisitos actualizados

---
+++ webPage_V1/requisiteswebPage.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+composer require sensio/framework-extra-bundle
+composer require jms/serializer-bundle
+composer require friendsofsymfony/rest-bundle
+composer require symfony/http-client
